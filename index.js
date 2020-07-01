2fb79851b6e402be541f3335493b5343c0cccd62
Vinícius Sidrim <47934208+Viniciussidrim@users.noreply.github.com>
2020-07-01 00:58:29 -0300

    Aula 1 a 3
    Variável e Atributos

---
+++ index.js
@@ -0,0 +1,17 @@
+//Curso Introdução a linguagem JavaScript - DevMedia
+
+// Aula 1
+function ornitorinco (morango, laranja){
+    return morango + laranja;
+}
+
+//Aula 2
+//Somente motivos para aprender JS
+
+//Aula 3
+var aluno = "jacks"; //Variavél normal
+
+aluno = { //Variavel Objeto
+    nome: "Jacks Douglas",
+    idade: 23
+}
