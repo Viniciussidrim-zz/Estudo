86f9530b19a4082e190ca1dab5de47fab91727a1
Vinícius Sidrim <viniciussidrim99@gmail.com>
2020-08-10 16:15:06 -0300

    Alteração de informações no Array

--- Constantes.js
+++ Constantes.js
@@ -1,2 +1,11 @@
 var veiculos = ["Fiat", "Ford", "Chevrollet"];
-console.log (veiculos);
+//console.log (veiculos);
+
+//Alterar a FORD por Citroen
+
+var veiculoExtra = "Citroen";
+veiculos[1] = veiculoExtra;
+//console.log (veiculos);
+
+//Armazenar diferentes dados no Array
+var informacoes = ["Fiat", 5.800, true];
