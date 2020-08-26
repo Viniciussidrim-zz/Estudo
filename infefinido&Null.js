7addec5a17d278ed3602721efa20e6ba14678bbd
Vinicius Sidrim <viniciussidrim99@gmail.com>
2020-08-25 21:26:08 -0300

    Variaveis

---
+++ infefinido&Null.js
@@ -0,0 +1,11 @@
+//Variavel indefinida
+var nome = "";
+//console.log (nome);
+
+//Variavel nula
+var valor = null;
+//console.log (valor+1);
+
+//Variavel que nunca muda o valor
+const url = "https://google.com.br";
+console.log (url);
