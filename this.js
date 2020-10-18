25baa4bace44fd5b7abeb757c08c26728d887a3a
Vinícius Sidrim <47934208+Viniciussidrim@users.noreply.github.com>
2020-10-17 21:35:16 -0300

    Update this.js

--- this.js
+++ this.js
@@ -1,7 +1,14 @@
-funcition Pessoa (nome, sobrenome){
+function Pessoa (nome, sobrenome){
     this.nome = nome;
     this.sobrenome = sobrenome;
     this.falar = function () {
         return ("Meu nome é " + this.nome + " " + this.sobrenome);
     }
 }
+
+// Criando um objeto a partir de uma função cosntrutora
+var funcionario = new Pessoa ("Maria", "Peixoto");
+
+// Acessando um objeto a partir de uma função contrutora
+funcionario.nome;
+funcionario.falar();
