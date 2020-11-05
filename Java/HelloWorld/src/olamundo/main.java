59dc290f7a5b89d69d3a31d7c398e680d06606db
Vinícius Sidrim <47934208+Viniciussidrim@users.noreply.github.com>
2020-11-05 19:36:08 -0300

    Introdução Curso Java

---
+++ Java/HelloWorld/src/olamundo/main.java
@@ -0,0 +1,13 @@
+package olamundo;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class main {
+
+    public static void main(String[] args){
+
+        System.out.println("Olá mundo");
+
+    }
+
+}
