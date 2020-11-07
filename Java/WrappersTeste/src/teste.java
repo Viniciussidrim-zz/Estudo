50ebf1983fe04551240b9fe393c3bbe63e6cb460
Vinícius Sidrim <47934208+Viniciussidrim@users.noreply.github.com>
2020-11-07 16:27:25 -0300

    Wrappers - Teste

---
+++ Java/WrappersTeste/src/teste.java
@@ -0,0 +1,14 @@
+public class teste {
+
+    public static void main (String[] args){
+
+        char charPrimitivo = 'ç';
+        int intPrimitivo = 100;
+        short shortPrimitivo = 6;
+
+        Character characterWhapper = 'ç';
+        
+
+    }
+
+}
